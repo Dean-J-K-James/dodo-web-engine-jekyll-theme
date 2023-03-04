3e2490790407f24936a88ed30bb17bf8c0da0765
Dean James <dean.j.k.james@gmail.com>
2023-03-04 15:59:56 +0000

    Removed heading includes in favour of heading tags

--- assets/js/slick.js
+++
@@ -1,8 +0,0 @@
-if (typeof Cookies.get('cookies-accept') === 'undefined') {
-    $('#cook').removeClass('!hidden');
-}
-
-$('#cookies-accept').click(function () {
-    Cookies.set('cookies-accept', true);
-    location.reload();
-});
